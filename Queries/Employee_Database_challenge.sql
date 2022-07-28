c9d3f8e224d60aabaf51adf31e0230515a4befbf
rafael24silva <106933029+rafael24silva@users.noreply.github.com>
2022-07-28 18:01:08 -0400

    Added challenge.

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,49 @@
+--Creating retirement titles table (Deliverable 1)
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE (to_date = '9999-01-01')
+ORDER BY rt.emp_no , rt.to_date DESC;
+
+--Retrieving employees by their most recent job title who are retiring
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.emp_no) DESC;
+
+--Creating a mentorship eligibility table (Deliverable 2)
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+INNER JOIN titles AS ti
+	ON e.emp_no = ti.emp_no
+WHERE (de.to_date = '9999-01-01' AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
